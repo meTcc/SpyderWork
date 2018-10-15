35d43da57cd6f278cfa8005db38cfb9f22e6492d
cctan <metcc@qq.com>
2018-10-15 19:37:26 +0800

    original fib using recusive

---
+++ fib.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 19:32:31 2018
+
+@author: tan
+"""
+
+class Fib(object):
+    def __init__(self):
+        pass
+    def s1(self, num):
+        if num < 2:
+            return num
+        return self.s1(num- 1) + self.s1(num - 2)
+    
+    
+if __name__ == '__main__':
+    man = Fib()
+    print(man.s1(5))
